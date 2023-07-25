4aafb8fceef1b749ccd9746900431ffeb775ae4b
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-07-25 18:41:49 +0300

    Update Main.java

--- src/main/java/org/example/spring/Main.java
+++ src/main/java/org/example/spring/Main.java
@@ -2,7 +2,7 @@ package org.example.spring;
 
 public class Main {
     public static void main(String[] args) {
-        Dog dog = new Dog();
-        dog.say();
+        Pet pet = new Cat();
+        pet.say();
     }
 }
