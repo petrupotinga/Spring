281c4daaacdbf56e7bc58024d7a80260b2225c78
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-07-26 22:27:26 +0300

    Create Person.java

---
+++ src/main/java/org/example/spring/Person.java
@@ -0,0 +1,15 @@
+package org.example.spring;
+
+public class Person {
+    private Pet pet;
+
+    public Person(Pet pet) {
+        this.pet = pet;
+    }
+
+    public void callYourPet() {
+        System.out.println("Hello my lovely Pet!");
+        pet.say();
+    }
+
+}
