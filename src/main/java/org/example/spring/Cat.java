a6bbf09130ede4244dcaf9b9d421423ea271f3cb
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-07-28 14:50:07 +0300

    Update Cat.java

--- src/main/java/org/example/spring/Cat.java
+++ src/main/java/org/example/spring/Cat.java
@@ -1,6 +1,10 @@
 package org.example.spring;
 
 public class Cat implements Pet {
+    public Cat() {
+        System.out.println("Cat bean is created");
+    }
+
     @Override
     public void say() {
         System.out.println("Meow-meow");
