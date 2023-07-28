291554e4b5d92554a77cd676e29ee5af44796983
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-07-28 14:50:13 +0300

    Update Dog.java

--- src/main/java/org/example/spring/Dog.java
+++ src/main/java/org/example/spring/Dog.java
@@ -1,6 +1,10 @@
 package org.example.spring;
 
 public class Dog implements Pet {
+    public Dog() {
+        System.out.println("Dog bean is created");
+    }
+
     public void say(){
         System.out.println("Bow-Wow!!");
     }
