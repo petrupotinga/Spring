a5e18e8ce78a0834330aa83aac9960e689663b68
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-07-30 10:50:17 +0300

    Update Test3.java

--- src/main/java/org/example/spring/Test3.java
+++ src/main/java/org/example/spring/Test3.java
@@ -10,6 +10,8 @@ public class Test3 {
 //        Pet pet = context.getBean("myPet", Pet.class);
         Person person = context.getBean("myPerson", Person.class);
         person.callYourPet();
+        System.out.println(person.getSurname());
+        System.out.println(person.getAge());
         context.close();
     }
 }
