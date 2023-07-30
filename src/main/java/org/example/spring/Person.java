a7819a1f35c3749b22c16c166aeb1da3c97ed1bd
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-07-30 10:50:01 +0300

    Update Person.java

--- src/main/java/org/example/spring/Person.java
+++ src/main/java/org/example/spring/Person.java
@@ -2,6 +2,8 @@ package org.example.spring;
 
 public class Person {
     private Pet pet;
+    private String surname;
+    private int age;
 
     public Person() {
         System.out.println("Person bean is createtd");
@@ -10,6 +12,24 @@ public class Person {
     public void setPet(Pet pet) {
         System.out.println("Class Person: set Pet");
         this.pet = pet;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        System.out.println("Class Person: set surname");
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        System.out.println("Class Person: set age");
+        this.age = age;
     }
 
     public void callYourPet() {
