97d125940e5a49f99059e2871c54b6ec247a750f
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-07-31 16:18:58 +0300

    Update Test4.java

--- src/main/java/org/example/spring/Test4.java
+++ src/main/java/org/example/spring/Test4.java
@@ -7,12 +7,12 @@ public class Test4 {
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("applicationContext2.xml");
         Dog myDog = context.getBean("myPet", Dog.class);
-        myDog.setName("Belka");
-        Dog yourDog = context.getBean("myPet", Dog.class);
-        yourDog.setName("Strelka");
-        System.out.println(myDog.getName());
-        System.out.println(yourDog.getName());
-        System.out.println("variabilele se referă la același obiect?: " + (myDog == yourDog));
+//        myDog.setName("Belka");
+//        Dog yourDog = context.getBean("myPet", Dog.class);
+//        yourDog.setName("Strelka");
+//        System.out.println(myDog.getName());
+//        System.out.println(yourDog.getName());
+//        System.out.println("variabilele se referă la același obiect?: " + (myDog == yourDog));
 
         context.close();
     }
