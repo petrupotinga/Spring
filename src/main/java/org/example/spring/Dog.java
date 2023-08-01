bbff9dcc654140f02e900037837d33cfb295d1b3
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-08-01 09:17:29 +0300

    Update Dog.java

--- src/main/java/org/example/spring/Dog.java
+++ src/main/java/org/example/spring/Dog.java
@@ -1,21 +1,28 @@
 package org.example.spring;
 
 public class Dog implements Pet {
-    private String name;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
+//    private String name;
+//    public String getName() {
+//        return name;
+//    }
+//    public void setName(String name) {
+//        this.name = name;
+//    }
 
     public Dog() {
         System.out.println("Dog bean is created");
     }
 
-    public void say(){
+    public void init() {
+        System.out.println("Class Dog: init method");
+    }
+
+    public void destroy() {
+        System.out.println("Class Dog: destroy method");
+    }
+
+    @Override
+    public void say() {
         System.out.println("Bow-Wow!!");
     }
 }
