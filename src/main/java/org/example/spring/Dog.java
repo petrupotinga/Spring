ae8c121ab3ffb427c34e555083a7221f2a106f60
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-08-02 21:04:14 +0300

    Update Dog.java

--- src/main/java/org/example/spring/Dog.java
+++ src/main/java/org/example/spring/Dog.java
@@ -1,5 +1,8 @@
 package org.example.spring;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Dog implements Pet {
 //    private String name;
 //    public String getName() {
