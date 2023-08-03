e4f6303466c968ae6e085178afdf1597dc3ed692
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-08-03 10:00:21 +0300

    Update Cat.java

--- src/main/java/org/example/spring/Cat.java
+++ src/main/java/org/example/spring/Cat.java
@@ -1,5 +1,8 @@
 package org.example.spring;
 
+import org.springframework.stereotype.Component;
+
+@Component("catBean")
 public class Cat implements Pet {
     public Cat() {
         System.out.println("Cat bean is created");
