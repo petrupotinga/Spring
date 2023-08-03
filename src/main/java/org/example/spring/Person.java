8be3e0a92a350bc912ecb4243a3222328ec857da
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-08-03 14:00:20 +0300

    Update Person.java

--- src/main/java/org/example/spring/Person.java
+++ src/main/java/org/example/spring/Person.java
@@ -5,26 +5,26 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-@Component
+@Component("personBean")
 public class Person {
-    @Autowired
-    @Qualifier("dog")
+//    @Autowired
+//    @Qualifier("dog")
     private Pet pet;
-    @Value("${person.surname}")
+//    @Value("${person.surname}")
     private String surname;
-    @Value("${person.age}")
+//    @Value("${person.age}")
     private int age;
 
 
-    public Person() {
-        System.out.println("Person bean is createtd");
-    }
-
-    //    @Autowired
-//    public Person(Pet pet) {
-//        System.out.println("Person bean is created");
-//        this.pet = pet;
+//    public Person() {
+//        System.out.println("Person bean is createtd");
 //    }
+
+        @Autowired
+    public Person(@Qualifier("catBean") Pet pet) {
+        System.out.println("Person bean is created");
+        this.pet = pet;
+    }
 
     public void setPet(Pet pet) {
         System.out.println("Class Person: set Pet");
