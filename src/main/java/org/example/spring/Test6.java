f362e4ec4322a53797b5cdad05bb963deb5b4f3a
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-08-03 13:55:52 +0300

    Create Test6.java

---
+++ src/main/java/org/example/spring/Test6.java
@@ -0,0 +1,14 @@
+package org.example.spring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Test6 {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(MyConfig.class);
+        Person person = context.getBean("personBean", Person.class);
+        person.callYourPet();
+        context.close();
+    }
+}
