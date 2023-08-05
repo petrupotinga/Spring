da097cd3f20a625ace4578bb99b59ee979079459
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-08-05 17:48:34 +0300

    Create Test1.java

---
+++ src/main/java/Hibernate/Entity/Test1.java
@@ -0,0 +1,24 @@
+package Hibernate.Entity;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Test1 {
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .buildSessionFactory();
+       try{
+           Session session = factory.getCurrentSession();
+           Employee employee = new Employee("Zaur", "Tregulov", "IT", 500);
+           session.beginTransaction();
+//           session.save(employee);
+           session.getTransaction().commit();
+       }
+       finally{
+           factory.close();
+       }
+    }
+}
