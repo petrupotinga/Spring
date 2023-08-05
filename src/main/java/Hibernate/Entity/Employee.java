cf0e3388f853c0d69060f0fe94446dffcc892be6
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-08-05 17:48:27 +0300

    Create Employee.java

---
+++ src/main/java/Hibernate/Entity/Employee.java
@@ -0,0 +1,83 @@
+package Hibernate.Entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+    @Id
+    @Column(name = "id")
+    private int id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "surname")
+    private String surname;
+    @Column(name = "departement")
+    private String departament;
+    @Column(name = "salary")
+    private int salary;
+
+    public Employee() {
+    }
+
+    public Employee(String name, String surname, String departament, int salary) {
+        this.name = name;
+        this.surname = surname;
+        this.departament = departament;
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", departament='" + departament + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getDepartament() {
+        return departament;
+    }
+
+    public void setDepartament(String departament) {
+        this.departament = departament;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+}
