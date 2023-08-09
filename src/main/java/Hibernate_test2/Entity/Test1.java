692efef55a42ad1dba282954d5a79956160614e4
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-08-09 12:32:20 +0300

    Create Test1.java

---
+++ src/main/java/Hibernate_test2/Entity/Test1.java
@@ -0,0 +1,35 @@
+package Hibernate_test2.Entity;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Test1 {
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .addAnnotatedClass(Details.class)
+                .buildSessionFactory();
+        try {
+//            Session session = factory.getCurrentSession();
+//            Employee employee = new Employee("Zaur", "Trigulov", "IT", 500);
+//            Details details = new Details("Baku", "07954875", "zaurtrigulov@gmail.com");
+//            employee.setEmpDetails(details);
+//            session.beginTransaction();
+//            session.save(employee);
+//            session.getTransaction().commit();
+//            System.out.println("Done!");
+            Session session = factory.getCurrentSession();
+            Employee employee = new Employee("Zaur", "Trigulov", "IT", 500);
+            Details details = new Details("Baku", "07954875", "zaurtrigulov@gmail.com");
+            employee.setEmpDetails(details);
+            session.beginTransaction();
+            session.save(employee);
+            session.getTransaction().commit();
+            System.out.println("Done!");
+        } finally {
+            factory.close();
+        }
+    }
+}
