b636251a62f9fed7a7acf60f8b672c5626f71713
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-08-12 22:41:52 +0300

    Create Test2.java

---
+++ src/main/java/Hibernate_test/OneToOne/Test2.java
@@ -0,0 +1,47 @@
+package Hibernate_test.OneToOne;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Test2 {
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .addAnnotatedClass(Details.class)
+                .buildSessionFactory();
+        Session session = null;
+        try {
+//            session = factory.getCurrentSession();
+//            Employee employee = new Employee("Misha", "Sidorov", "HR", 850);
+//            Details details = new Details("London", "26464544", "mishanya@gmail.com");
+//            employee.setEmpDetails(details);
+//            details.setEmployee(employee);
+//            session.beginTransaction();
+//            session.save(details);
+//            session.getTransaction().commit();
+//            System.out.println("Done!");
+
+//            session = factory.getCurrentSession();
+//            session.beginTransaction();
+//            Details details = session.get(Details.class, 3);
+//            System.out.println(details.getEmployee());
+//            session.getTransaction().commit();
+//            System.out.println("Done!");
+
+            session = factory.getCurrentSession();
+            session.beginTransaction();
+            Details details = session.get(Details.class, 3);
+//            details.getEmployee().setEmpDetails(null);
+            session.delete(details);
+            session.getTransaction().commit();
+            System.out.println("Done!");
+
+        } finally {
+            session.close();
+            factory.close();
+        }
+    }
+}
+
