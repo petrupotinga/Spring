9ed12973ccaeadd1d77173ce5750ea81cb7afdb6
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-08-14 19:22:08 +0300

    Create Test1.java

---
+++ src/main/java/Hibernate_OneToMany_bi/Entity/Test1.java
@@ -0,0 +1,32 @@
+package Hibernate_OneToMany_bi.Entity;
+
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Test1 {
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .addAnnotatedClass(Department.class)
+                .buildSessionFactory();
+        Session session = null;
+        try {
+            session = factory.getCurrentSession();
+            Department dep = new Department("IT", 300, 1200);
+            Employee emp1 = new Employee("Zaur", "Trigulov", 600);
+            Employee emp2 = new Employee("Elena", "Smirnova", 1000);
+            dep.addEmployeeToDepartment(emp1);
+            dep.addEmployeeToDepartment(emp2);
+            session.beginTransaction();
+            session.save(dep);
+            session.getTransaction().commit();
+            System.out.println("Done!");
+        } finally {
+            session.close();
+            factory.close();
+        }
+    }
+}
