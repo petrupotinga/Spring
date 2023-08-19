9b07db599d47f796956145322350fbef72c6a29c
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-08-19 14:15:21 +0300

    Update Department.java

--- src/main/java/Hibernate_OneToMany_bi/Entity/Test/Department.java
+++ src/main/java/Hibernate_OneToMany_bi/Entity/Test/Department.java
@@ -17,7 +17,7 @@ public class Department {
     private int maxSalary;
     @Column(name = "min_salary")
     private int minSalary;
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "department")
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "department", fetch = FetchType.EAGER)
     private List<Employee> emps;
 
 
